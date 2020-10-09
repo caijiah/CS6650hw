de47911bc315d65176c19e753ddf8715fb544ab2
Jiahao Cai <cai.jiah@husky.neu.edu>
2020-10-09 11:54:32 -0400

    update ServerMain.cpp

--- assignment01/src/ServerMain.cpp
+++ assignment01/src/ServerMain.cpp
@@ -1,29 +1,70 @@
+#include "ServerSocket.h"
 #include "ServerStub.h"
-#include "RobotInfo.h"
-#include "Order.h"
 #include <iostream>
-#include <memory>
 #include <list>
 #include <thread>
 #include <queue>
 #include <mutex>
-#include <chrono>
 #include <map>
-#include <condition_variable>
+#include <chrono>
 
-std::mutex queue_mtx;
-std::mutex map_mtx;
-std::condition_variable expert_cv;
-std::condition_variable engineer_cv;
-std::map<unsigned int, int> engineer_expert_pair;
-std::queue<unsigned int> expert_reqs_queue;
+using namespace std;
 
-int str_to_int(char* arg) {
+mutex que_mtx;
+queue<unsigned int> shared_reqs_queue;
+
+mutex map_mtx;
+map<unsigned int, int>engineer_assign_expert;
+
+condition_variable expert_cv;
+condition_variable engineer_cv;
+
+// help function that simply use stoi but with error handling
+int string_to_int(std::string str) {
     try {
-        return std::stoi(arg);
+        return std::stoi(str);
     } catch (std::invalid_argument& e) {
-        std::cerr << "Error: Fail to parse " << arg << " to an interger." << std::endl;
+        std::cerr << "Error: Invalid input: " << str
+        << ". Please check." << std::endl;
         exit(1);
+    }
+}
+
+void process_orders(
+                    std::unique_ptr<ServerStub> ser_stub,
+                    unsigned int engineer_id
+                    ) {
+    while (true) {
+        std::unique_ptr<Order> order;
+        
+        try {
+            order = ser_stub->ReceiveOrder();
+        } catch (const char* msg) {
+            break;
+        }
+        
+        int expert_id = -1;
+        if (order->need_an_expert()) {
+            // lock, since we push reqs to the shared queue
+            que_mtx.lock();
+            shared_reqs_queue.push(engineer_id);
+            que_mtx.unlock();
+            expert_cv.notify_one();
+            
+            while (true) {
+                unique_lock<mutex> map_lock(map_mtx);
+                auto assign_itr = engineer_assign_expert.find(engineer_id);
+                if (assign_itr == engineer_assign_expert.end()) {
+                    engineer_cv.wait(map_lock);
+                } else {
+                    expert_id = assign_itr->second;
+                    engineer_assign_expert.erase(engineer_id);
+                    map_lock.unlock();
+                    break;
+                }
+            }
+        }
+        ser_stub->ShipRobot(RobotInfo(*order, engineer_id, expert_id));
     }
 }
 
@@ -31,89 +72,51 @@ void expert_process_orders(
                            unsigned int expert_id
                            ) {
     while (true) {
-        std::unique_lock<std::mutex> ul(queue_mtx);
-        if (expert_reqs_queue.size() == 0) {
-            // Auto release the lock and wait indefinitely until being signled.
-            expert_cv.wait(ul); // Auto hold the lock after wake up.
+        unique_lock<std::mutex> que_lock(que_mtx);
+        if (shared_reqs_queue.size() == 0) {
+            expert_cv.wait(que_lock);
         } else {
-            int engineer_id = expert_reqs_queue.front();
-            expert_reqs_queue.pop();
-            ul.unlock();
-            std::this_thread::sleep_for(std::chrono::microseconds(100));
+            int engineer_id = shared_reqs_queue.front();
+            shared_reqs_queue.pop();
+            que_lock.unlock();
+            this_thread::sleep_for(chrono::microseconds(100));
             map_mtx.lock();
-            engineer_expert_pair.insert({engineer_id, expert_id});
+            engineer_assign_expert.insert({engineer_id, expert_id});
             map_mtx.unlock();
             engineer_cv.notify_all();
         }
     }
 }
 
-void process_orders(
-                    std::unique_ptr<ServerStub> stub,
-                    unsigned int engineer_id
-                    ) {
-    while (true) {
-        std::unique_ptr<Order> order;
-        
-        try {
-            order = stub->receive_order();
-        } catch (const char* msg) {
-            break;
-        }
-        
-        int expert_id = -1;
-        
-        if (order->is_expert_required()) {
-            queue_mtx.lock();
-            expert_reqs_queue.push(engineer_id);
-            queue_mtx.unlock();
-            expert_cv.notify_one();
-            
-            while (true) {
-                std::unique_lock<std::mutex> ml(map_mtx);
-                auto pair_itr = engineer_expert_pair.find(engineer_id);
-                if (pair_itr == engineer_expert_pair.end()) {
-                    engineer_cv.wait(ml);
-                } else {
-                    expert_id = pair_itr->second;
-                    engineer_expert_pair.erase(engineer_id);
-                    ml.unlock();
-                    break;
-                }
-            }
-        }
-        
-        stub->ship_robot(RobotInfo(*order, engineer_id, expert_id));
-    }
-}
-
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        std::cerr << "Error: please provide [port #] [# experts]." << std::endl;
+        std::cerr << "Please type these 2 arguments: [port #]"
+        << " [# experts]" << std::endl;
         exit(1);
     }
-    int portno = str_to_int(argv[1]);
-    int num_of_experts = str_to_int(argv[2]);
     
-    std::list<std::thread> experts;
-    std::list<std::thread> engineers;
-    ServerSocket sock(portno);
+    int port_num = string_to_int(argv[1]);
+    ServerSocket sock = ServerSocket(port_num);
+    int experts_num = string_to_int(argv[2]);
+    
+    list<thread> engineers;
+    list<thread> experts_pool;
+    
     int engineer_id = 0;
     
-    // Create threads for expert engineers.
-    for (int i = 0; i < num_of_experts; i++) {
-        experts.push_back(std::thread(expert_process_orders, engineer_id));
+    for (int i = 0; i < experts_num; i++) {
+        experts_pool.push_back(std::thread(expert_process_orders, engineer_id));
         engineer_id++;
     }
     
-    int counter = 0;
     while (true) {
-        int conn_sock = sock.accept();
-        counter++;
-        std::unique_ptr<ServerStub> stub = std::unique_ptr<ServerStub>(new ServerStub(conn_sock));
-        engineers.push_back(std::thread(process_orders, std::move(stub), engineer_id));
+        int connected_new_fd = sock.ser_sock_accept();
+        unique_ptr<ServerStub> ser_stub;
+        ser_stub->Init(connected_new_fd);
+        engineers.push_back(thread(process_orders, move(ser_stub), engineer_id));
         engineer_id++;
     }
     
     return 0;
 }
+
