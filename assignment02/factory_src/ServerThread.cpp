0f44599189e472af1fa06296ad54361322633964
Jiahao Cai <cai.jiah@husky.neu.edu>
2020-10-28 23:04:42 -0400

    remove pirnts

--- assignment02/factory_src/ServerThread.cpp
+++ assignment02/factory_src/ServerThread.cpp
@@ -55,8 +55,7 @@ void RobotFactory::EngineerThread(std::unique_ptr<ServerSocket> socket, int id) 
 		identify_message = server_stub.ReceiveIdentifyMessage();
 		int indentify_flag;
 		indentify_flag = identify_message.GetIdentifyFlag();
-		std::cout << "receive a indentify_flag" << indentify_flag << std::endl;
-		// std::cout << "receive a indentify_flag" << std::endl;
+		// std::cout << "receive a indentify_flag" << indentify_flag << std::endl;
 		switch (indentify_flag) {
 			case CLIENT:
 			{
@@ -69,17 +68,16 @@ void RobotFactory::EngineerThread(std::unique_ptr<ServerSocket> socket, int id) 
 					switch (request_type) {
 						case 1:
 						{
-							std::cout << "before create" << std::endl;
+							// std::cout << "before create" << std::endl;
 							robot = CreateRobotAndAdminRequest(crq, engineer_id);
-							std::cout << "Got a robot" << robot.GetCustomerId() << std::endl;
+							// std::cout << "Got a robot" << robot.GetCustomerId() << std::endl;
 							server_stub.ShipRobot(robot);
 							break;
 						}
 						case 2:
 						{
 							cid = crq.GetCustomerId();
-							// defaul
-							std::cout << "Got read request" << cid << std::endl;
+							// std::cout << "Got read request" << cid << std::endl;
 
 							crd.SetCustomerId(cid);
 							crd.SetLastOrder(-1);
@@ -89,7 +87,7 @@ void RobotFactory::EngineerThread(std::unique_ptr<ServerSocket> socket, int id) 
 								crd.SetLastOrder(customer_record[cid]);
 							}
 							crd_lock.unlock();
-							std::cout << "record: " << cid << " : " << crd.GetLastOrder() << std::endl;
+							// std::cout << "record: " << cid << " : " << crd.GetLastOrder() << std::endl;
 
 							server_stub.ReturnRecord(crd);
 							break;
@@ -107,7 +105,6 @@ void RobotFactory::EngineerThread(std::unique_ptr<ServerSocket> socket, int id) 
 					ReplicationRequest replica_req;
 					replica_req = server_stub.ReceiveReplicationRequest();
 					if (!replica_req.IsValid()) {
-						std::cout << "did I break?" << std::endl;
 						break;
 					}
 					int primary_factory = replica_req.GetFactoryId();
@@ -117,39 +114,35 @@ void RobotFactory::EngineerThread(std::unique_ptr<ServerSocket> socket, int id) 
 					// write log
 					int replica_last_index = replica_req.GetLastIndex();
 					MapOp replica_mop = replica_req.GetMapOp();
-					std::cout << replica_mop.GetOpcode() << std::endl;
-					std::cout << replica_mop.GetArg1() << std::endl;
-					std::cout << replica_mop.GetArg2() << std::endl;
-					std::cout << "commited order number " <<  replica_mop.GetArg2() << std::endl;
 
 					smr_log_lock.lock();
-					std::cout << "last index" << replica_last_index << std::endl;
+					// std::cout << "last index" << replica_last_index << std::endl;
 					if ((replica_last_index - admin_config.last_index) > 1) {
 						int temp_last = admin_config.last_index;
 						for (int i = temp_last; i < replica_last_index; i++) {
 							MapOp dummy_mapop = MapOp();
 							smr_log.insert((smr_log.begin() + i), dummy_mapop);
-							std::cout << "fix" << i << std::endl;
+							// std::cout << "fix" << i << std::endl;
 							admin_config.last_index += 1;
 						}
-						std::cout << "fixed" << admin_config.last_index << std::endl;
+						// std::cout << "fixed" << admin_config.last_index << std::endl;
 					}
 					smr_log.insert((smr_log.begin() + replica_last_index), replica_mop);
-					std::cout << "check order number " << smr_log[replica_last_index].GetArg2() << std::endl;
-					std::cout << "after set smr_log" << std::endl;
+					// std::cout << "check order number " << smr_log[replica_last_index].GetArg2() << std::endl;
+					// std::cout << "after set smr_log" << std::endl;
 					smr_log_lock.unlock();
 					admin_config.last_index = replica_last_index;
 					// Applies MapOp in the req.committed index of smr log to the customer record and
 					// update self.committed index; and
 					int replica_commited_index = replica_req.GetCommittedIndex();
-					std::cout << "commited index" << replica_commited_index << std::endl;
+					// std::cout << "commited index" << replica_commited_index << std::endl;
 					if (replica_commited_index != -1) {
 						smr_log_lock.lock();
 						MapOp op_replica_commited = smr_log[replica_commited_index];
 						smr_log_lock.unlock();
 						int commited_op_cid = op_replica_commited.GetArg1();
 						int commited_op_order_num = op_replica_commited.GetArg2();
-						std::cout << "commited order number " <<  commited_op_order_num << std::endl;
+						// std::cout << "commited order number " <<  commited_op_order_num << std::endl;
 						crd_lock.lock();
 						if (customer_record.find(commited_op_cid) == customer_record.end()) {
 							customer_record.insert({commited_op_cid, commited_op_order_num});
@@ -165,7 +158,7 @@ void RobotFactory::EngineerThread(std::unique_ptr<ServerSocket> socket, int id) 
 			}
 			default:
 				std::cout << "Undefined identity type: " << indentify_flag << std::endl;
-				// break;
+				break;
 		}
 }
 
@@ -179,7 +172,7 @@ void RobotFactory::updateCusRecord() {
 	} else {
 		customer_record[last_op_cid] = last_op_order_num;
 	}
-	std::cout << "written record"<< last_op_cid << " : " << customer_record[last_op_cid] << std::endl;
+	// std::cout << "written record"<< last_op_cid << " : " << customer_record[last_op_cid] << std::endl;
 	admin_config.committed_index = admin_config.last_index;
 	crd_lock.unlock();
 }
@@ -192,20 +185,16 @@ void RobotFactory::AdminThread(int id) {
 		if (arq.empty()) {
 			arq_cv.wait(ul, [this]{ return !arq.empty(); });
 		}
-		std::cout << "receive done?" << std::endl;
 		// received customer record update request
 		admin_config_lock.lock();
 		if (admin_config.primary_id != admin_config.unique_id) {
 			if (admin_config.primary_id != -1 ) {
-				std::cout << "primary_id"<< admin_config.primary_id << std::endl;
-				std::cout << "might enter here???" << std::endl;
 				updateCusRecord();
 				std::this_thread::sleep_for(std::chrono::microseconds(100));
 			}
 			admin_config.primary_id = admin_config.unique_id;
 		}
 		admin_config_lock.unlock();
-		std::cout << "set primary id done" << std::endl;
 
 		if (!peer_connected) {
 			// make connections...
@@ -214,19 +203,15 @@ void RobotFactory::AdminThread(int id) {
 				Peer peer = it->second;
 				if (peer.GetPeerStatus()) {
 					std::unique_ptr<FactoryStub> factory_stub = std::unique_ptr<FactoryStub>(new FactoryStub);
-					std::cout << "FactoryStub init" << std::endl;
 					if (!factory_stub->Init(peer.GetPeerIP(), peer.GetPeerPort())) {
 						peer.SetPeerStatus(false);
-						std::cout << "Peer " << peer.GetPeerID() << " failed to connect" << std::endl;
 					} else {
 						peer_connections[peer.GetPeerID()] = std::move(factory_stub);
 					}
 				}
 			}
-				std::cout << "make connections?" << std::endl;
 			peer_connected = true;
 		}
-		std::cout << "make connections done?" << std::endl;
 
 		auto req = std::move(arq.front());
 		arq.pop();
@@ -248,10 +233,6 @@ void RobotFactory::AdminThread(int id) {
 		}
 		smr_log_lock.unlock();
 
-		std::cout << admin_config.last_index << std::endl;
-		std::cout << "last_index:" << admin_config.last_index << std::endl;
-
-
 		ReplicationRequest replica_req;
 		replica_req.SetRequest(admin_config.factory_id, admin_config.committed_index
 			                   , admin_config.last_index, op);
@@ -260,7 +241,6 @@ void RobotFactory::AdminThread(int id) {
 		for (it = peer_connections.begin(); it != peer_connections.end(); it++ ) {
 			// std::unique_ptr<FactoryStub> each_stub = std::move(it->second);
 			if (!it->second->GetStubDown()) {
-				std::cout << "send " << std::endl;
 				if (!it->second->GetIdentified()) {
 					IdentifyMessage identify_message;
 					identify_message.SetIdentifyFlag(PFA);
@@ -269,7 +249,6 @@ void RobotFactory::AdminThread(int id) {
 				}
 				int res = it->second->SendReplicationRequest(replica_req);
 				if (res != admin_config.last_index) {
-					std::cout << "One back up failed: " << it->first << std::endl;
 					// handle error
 					it->second->ShutStubDown();
 					// return;
